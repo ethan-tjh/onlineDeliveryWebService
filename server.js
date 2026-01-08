a1e5753a69763de2e4ff475b8188c1c21eb270f3
ethan-tjh <ethantan905@gmail.com>
2026-01-08 12:02:30 +0800

    Initial Commit

---
+++ server.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const mysql = require('mysql2/promise');
+require('dotenv').config();
+const port = 3000;
+const dbConfig = {
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT,
+    waitForConnections: true,
+    connectionLimit: 100,
+    queueLimit: 0,
+};
+const app = express();
+app.use(express.json());
+app.listen(port, () => {
+    console.log('Server running on port', port);
+});
+// GET
+app.get('/allDeliveries', async (req, res) => {
+    try {
+        let connection = await mysql.createConnection(dbConfig);
+        const [rows] = await connection.execute('SELECT * FROM defaultdb.deliveries');
+        res.json(rows);
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({message: 'Server error for allDeliveries'});
+    }
+});
+// POST
+app.post('/addDeliveries', async (req, res) => {
+    const {fullname, phone_num, delivery_status} = req.body;
+    try {
+        let connection = await mysql.createConnection(dbConfig);
+        await connection.execute('INSERT INTO deliveries (fullname, phone_num, delivery_status) VALUES (?, ?, ?)', [fullname, phone_num, delivery_status]);
+        res.status(201).json({message: 'Delivery for ' + fullname + ' has been added successfully'});
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({message: 'Server error - could not add Delivery for ' + fullname});
+    }
+});
